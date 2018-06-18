c5e2854bc4bb8bb7d91195e4907f8fa95931f7a8
Radoslav Blasko <radoslav.blasko@gmail.com>
2018-06-18 19:06:52 +0200

    Initial Commit

---
+++ WhitbreadTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhitbreadTest
+//
+//  Created by Radoslav Blasko on 18/06/2018.
+//  Copyright © 2018 Radoslav Blasko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
