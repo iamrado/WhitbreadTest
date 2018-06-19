ce802c702e62889e617f7eb0de9eff8ef141cf07
Radoslav Blasko <radoslav.blasko@gmail.com>
2018-06-19 10:35:10 +0200

    Add Venues title

--- WhitbreadTest/AppDelegate.swift
+++ WhitbreadTest/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         let viewController: VenueSearchViewController = VenueSearchViewController.instantiate(api: api)
+        viewController.title = NSLocalizedString("Venues", comment: "")
         let navigationController = UINavigationController(rootViewController: viewController)
 
         window = UIWindow(frame: UIScreen.main.bounds)
